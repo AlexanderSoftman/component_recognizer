d04ab2ebbd87ca60e33130d1f82882bb7a214c0a
afomin <afomin@topcon.com>
2018-02-07 19:05:27 +0300

    initial dots searching in progress

--- component_recognizer/start_processing.py
+++ component_recognizer/start_processing.py
@@ -1,6 +1,15 @@
 import logging
-import settings
-import calibrator
+import component_recognizer.settings as settings
+import component_recognizer.calibrator as calibrator
+import component_recognizer.contour_searcher as contour_searcher
+import os
+import cv2
+
+test_image = cv2.imread(
+    os.path.join(
+        os.path.split(__file__)[0],
+        "pcb_images",
+        "pcb_1.jpg"))
 
 
 def main():
@@ -9,9 +18,15 @@ def main():
         format='%(asctime)s - %(levelname)s - %(message)s')
     calibrator_m = calibrator.Calibrator()
     settings_m = settings.Settings()
-    # get calibration file
-    camera_calibration = calibrator_m.get_calibration(
-        settings_m.calibration_file_folder)
+    contour_searcher_m = contour_searcher.ContourSearcher()
+    # get calibration
+    # camera_calibration = calibrator_m.get_calibration(
+        # settings_m.calibration_file_folder)
+    # test_image_calibrated = calibrator_m.apply_calibration(
+        # test_image,
+        # camera_calibration)
+    test_image_calibrated = test_image
+    contour_searcher_m.find_contour_dots(test_image_calibrated)
 
 
 if __name__ == '__main__':
